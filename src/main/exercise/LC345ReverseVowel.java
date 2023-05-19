43577710060141dd92f543a55b1b24df086dc253
Chen Hua <chua0221@gmail.com>
2023-05-19 10:25:35 -0700

    review LC345 reverse vowels of a string

--- src/main/exercise/LC345ReverseVowel.java
+++ src/main/exercise/LC345ReverseVowel.java
@@ -4,40 +4,43 @@ public class LC345ReverseVowel {
 
     public static String reverseVowels(String s) {
         if (s.length() < 2) return s;
-        char[] chars = new char[s.length()];
+        String[] res = new String[s.length()];
         String vowels = "aeiouAEIOU";
-        int l = 0;
-        int r = s.length() - 1;
-        while (l <= r) {
-            char charL = s.charAt(l);
-            char charR = s.charAt(r);
-            if (!vowels.contains(""+charL) && !vowels.contains(""+charR)) {
-                chars[l] = charL;
-                chars[r] = charR;
-                l++;
-                r--;
-            } else if (vowels.contains(""+charL) && !vowels.contains(""+charR)) {
-                chars[r] = charR;
-                r--;
-            } else if (!vowels.contains(""+charL) && vowels.contains(""+charR)) {
-                chars[l] = charL;
-                l++;
-            } else {
-                chars[r] = charL;
-                chars[l] = charR;
-                l++;
-                r--;
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left <= right) {
+            String cLeft = "" + s.charAt(left);
+            String cRight = "" + s.charAt(right);
+            if (!vowels.contains(cLeft) && !vowels.contains(cRight)) {
+                res[left] = cLeft;
+                res[right] = cRight;
+                left++;
+                right--;
+            } else if (vowels.contains(cLeft) && !vowels.contains(cRight)) {
+                res[right] = cRight;
+                right--;
+            } else if (!vowels.contains(cLeft) && vowels.contains(cRight)) {
+                res[left] = cLeft;
+                left++;
+            } else if (vowels.contains(cLeft) && vowels.contains(cRight)) {
+                res[left] = cRight;
+                res[right] = cLeft;
+                left++;
+                right--;
             }
         }
-        return String.valueOf(chars);
+        return String.join("", res);
     }
 
     public static void main(String[] args) {
         String s = "hello";
 //        char[] temp = new char[s.length()];
 //        System.out.println(temp.length);
+        System.out.println(LC345ReverseVowel.reverseVowels(",."));
         System.out.println(LC345ReverseVowel.reverseVowels("a."));
-
+        System.out.println(LC345ReverseVowel.reverseVowels("hello"));
+        System.out.println(LC345ReverseVowel.reverseVowels("leetcode"));
     }
 
 }
