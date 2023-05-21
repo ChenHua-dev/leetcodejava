4db6d7215e195b2c68211e0eebc3b874b756255e
Chen Hua <chua0221@gmail.com>
2023-05-20 21:00:11 -0700

    review LC1512 good pair

--- src/main/exercise/LC1512GoodPair.java
+++ src/main/exercise/LC1512GoodPair.java
@@ -1,24 +1,27 @@
 package exercise;
 
+import java.util.HashMap;
+
 public class LC1512GoodPair {
 
     public static int numIdenticalPairs(int[] nums) {
-        int n = nums.length;
         int counter = 0;
-        for (int i = 0; i < n - 1; i++) {
-            int currInt = nums[i];
-            for (int j = i + 1; j < n; j++) {
-                if (currInt == nums[j]) {
-                    counter++;
-                }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int curr : nums) {
+            if (!map.containsKey(curr)) {
+                map.put(curr, 1);
+            } else {
+                int rep = map.get(curr);
+                counter += rep;
+                map.put(curr, ++rep);
             }
         }
         return counter;
     }
 
     public static void main(String[] args) {
-//        System.out.println(LC1512GoodPair.numIdenticalPairs(new int[] {1,2,3,1,1,3}));
-//        System.out.println(LC1512GoodPair.numIdenticalPairs(new int[] {1,1,1,1}));
+        System.out.println(LC1512GoodPair.numIdenticalPairs(new int[] {1,2,3,1,1,3}));
+        System.out.println(LC1512GoodPair.numIdenticalPairs(new int[] {1,1,1,1}));
         System.out.println(LC1512GoodPair.numIdenticalPairs(new int[] {1,2,3}));
     }
 }
