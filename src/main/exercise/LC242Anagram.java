bab000e0ac48cf0f548c25c154c7d7317150207a
Chen Hua <chua0221@gmail.com>
2023-05-20 20:05:21 -0700

    review LC242 anagram

--- src/main/exercise/LC242Anagram.java
+++ src/main/exercise/LC242Anagram.java
@@ -6,40 +6,37 @@ import java.util.Set;
 public class LC242Anagram {
 
     public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
 
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char currChar = s.charAt(i);
-            if (!map.containsKey(currChar)) {
-                map.put(currChar, 1);
-            } else {
-                int count = map.get(currChar);
-                count++;
-                map.put(currChar, count);
-            }
-        }
-
+        HashMap<String, Integer> map = new HashMap<>();
         for (int i = 0; i < t.length(); i++) {
-            char newChar = t.charAt(i);
-            if(map.containsKey(newChar)) {
-                int count2 = map.get(newChar);
-                count2--;
-                map.put(newChar, count2);
+            String curr = Character.toString(t.charAt(i));
+            if (!map.containsKey(curr)) {
+                map.put(curr, 1);
             } else {
-                return false;
+                int count = map.get(curr);
+                map.put(curr, ++count);
             }
         }
 
-        Set<Character> set = map.keySet();
-        for (char c : set) {
-            if (map.get(c) != 0) return false;
+        for (int j = 0; j < s.length(); j++) {
+            String next = Character.toString(s.charAt(j));
+            if (map.containsKey(next)) {
+                int count2 = map.get(next);
+                map.put(next, --count2);
+            }
+        }
+
+        Set<String> set = map.keySet();
+        for (String str : set) {
+            if (map.get(str) != 0) return false;
         }
         return true;
     }
 
 
     public static void main(String[] args) {
+        System.out.println(LC242Anagram.isAnagram("anagram", "nagaram"));
         System.out.println(LC242Anagram.isAnagram("a", "ab"));
     }
 
