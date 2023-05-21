34a9dbd38a5e0c9c7506a356c7565052c0be5413
Chen Hua <chua0221@gmail.com>
2023-05-20 20:41:08 -0700

    review LC243 shortest word distance

--- src/main/exercise/LC243ShortestDist.java
+++ src/main/exercise/LC243ShortestDist.java
@@ -3,22 +3,22 @@ package exercise;
 public class LC243ShortestDist {
 
     public static int shortestDistance(String[] wordsDict, String word1, String word2) {
-        int minDist = Integer.MAX_VALUE;
-        int i1 = -1;
-        int i2 = -1;
+        int index1 = -1;
+        int index2 = -1;
+        int shortest = Integer.MAX_VALUE;
 
-        for (int i = 0; i< wordsDict.length; i++) {
-            String currWord = wordsDict[i];
-            if (currWord.equals(word1)) {
-                i1 = i;
-            } else if (currWord.equals(word2)) {
-                i2 = i;
+        for (int i = 0; i < wordsDict.length; i++) {
+            if (wordsDict[i].equals(word1)) {
+                index1 = i;
+            } else if (wordsDict[i].equals(word2)) {
+                index2 = i;
             }
-            if (i1 != -1 && i2 != -1) {
-                minDist = Math.min(minDist, Math.abs(i1 - i2));
+
+            if (index1 != -1 && index2 != -1) {
+                shortest = Math.min(shortest, Math.abs(index1 - index2));
             }
         }
-        return minDist;
+        return shortest;
     }
 
     public static void main(String[] args) {
