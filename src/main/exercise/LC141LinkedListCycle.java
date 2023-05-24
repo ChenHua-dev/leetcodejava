f77e5e24399b3688519c50ae3274262591893540
Chen Hua <chua0221@gmail.com>
2023-05-23 23:13:57 -0700

    add LC141 linked list cycle

---
+++ src/main/exercise/LC141LinkedListCycle.java
@@ -0,0 +1,25 @@
+package exercise;
+
+import model.ListNode;
+
+public class LC141LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        // 1st check fast != null, not: fast.next != null && fast.next.next != null
+        // b/c if head is [], then fast.next would throw null pointer exception
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
