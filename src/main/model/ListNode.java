8dcfd06d0bd3f60b2ad80846b93400afef52323d
Chen Hua <chua0221@gmail.com>
2023-05-29 00:30:25 -0700

    add create singly linked list from int array

--- src/main/model/ListNode.java
+++ src/main/model/ListNode.java
@@ -9,9 +9,19 @@ public class ListNode {
     public ListNode(int val) { this.val = val; }
     public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 
-    public static ListNode createLLFromArray(int[] arrayList) {
-
-        return null;
+    public static ListNode createLLFromArray(int[] arr) {
+        ListNode head;
+        if (arr.length == 0) {
+            return null;
+        } else {
+            head = new ListNode(arr[0]);
+            ListNode curr = head;
+            for (int i = 1; i < arr.length; i++) {
+                curr.next = new ListNode(arr[i]);
+                curr = curr.next;
+            }
+        }
+        return head;
     }
 
     public static String displayLinkedList(ListNode head) {
