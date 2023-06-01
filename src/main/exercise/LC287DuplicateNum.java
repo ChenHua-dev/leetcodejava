446476a7669eebacb2775c1d1c3c7d85a7f30513
Chen Hua <chua0221@gmail.com>
2023-05-31 21:22:20 -0700

    add LC287 find the only duplicate number

---
+++ src/main/exercise/LC287DuplicateNum.java
@@ -0,0 +1,25 @@
+package exercise;
+
+public class LC287DuplicateNum {
+
+    public static int findDuplicate(int[] nums) {
+        int dup = -1;
+        for (int i = 0; i < nums.length; i++) {
+            int curr = Math.abs(nums[i]);
+            if (nums[curr] < 0) {
+                dup = curr;
+                break;
+            } else {
+                nums[curr] = -1 * nums[curr];
+            }
+        }
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = Math.abs(nums[i]);
+        }
+        return dup;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
